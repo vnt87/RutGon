ac7e24a7aac49af2ddf32f17c216d2ff1d4c26df
Nam Vu <contact@namvu.net>
2025-01-10 00:01:53 +0700

    skip the useless landing page and go directly to the dashboard

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,6 +2,11 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (import.meta.server)
     return
 
+  // Redirect from landing page to dashboard
+  if (to.path === '/') {
+    return navigateTo('/dashboard')
+  }
+
   if (to.path.startsWith('/dashboard') && to.path !== '/dashboard/login') {
     if (!window.localStorage.getItem('SinkSiteToken'))
       return navigateTo('/dashboard/login')
